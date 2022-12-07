e0cb67f0d6314fbb279a9d4f711995416b4d3d6f
DaeHOHoHOHo <112150244+DaeHOHoHOHo@users.noreply.github.com>
2022-12-07 23:30:47 +0900

    Info.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
-import Counter2 from "./Counter2"
+import React, { useState } from "react";
+import Info from "./Info";
 
 const App = () => {
-  return <Counter2/>;
-}
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button onClick={() => {
+        setVisible(!visible);
+      }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  )
+};
+
 
 export default App;
